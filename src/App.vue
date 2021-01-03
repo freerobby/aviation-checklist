8b007bd01de192761b4b9c0206938269c075da84
Robby Grossman <robby@freerobby.com>
2021-01-02 22:26:50 -0500

    Remove template checklist.

--- src/App.vue
+++ src/App.vue
@@ -22,32 +22,7 @@ export default {
   },
   data() {
     return {
-      checklistSets: [{
-        title: 'Taxi',
-        checklists: [
-          {
-            title: 'Before Engine Start',
-            items: [
-              { id: 0, subject: 'Passenger Briefing', operation: 'Complete' },
-              { id: 1, subject: 'Seats & Seatbelts', operation: 'Secure' },
-              { id: 2, subject: 'Circuit Breakers', operation: 'Check' },
-              { id: 3, subject: 'Electrical Switches', operation: 'Verify Off'},
-              { id: 4, subject: 'Avionics Master', operation: 'Verify Off'},
-              { id: 5, subject: 'Fuel Selector', operation: 'Verify Both'},
-              { id: 6, subject: 'Flight Controls', operation: 'Check'},
-              { id: 7, subject: 'Brakes', operation: 'Check Pressure'}
-            ]
-          },
-          {
-            title: 'Taxi',
-            items: [
-              {id: 0, subject: 'Brakes', operation: 'Check Friction'},
-              {id: 1, subject: 'Flight Instruments', operation: 'Check'},
-              {id: 2, subject: 'Flight Controls', operation: 'Position for Wind'}
-            ]
-          }
-        ]
-      }]
+      checklistSets: []
     }
   },
   created: function() {
