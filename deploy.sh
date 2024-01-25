578159d27f7fb73c25e91de231d83715b4e8c682
Robby Grossman <robby@freerobby.com>
2024-01-25 11:40:22 -0500

    Use 'main' instead of 'master' for deploy branch.

--- deploy.sh
+++ deploy.sh
@@ -11,6 +11,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:freerobby/aviation-checklist.git master:gh-pages
+git push -f git@github.com:freerobby/aviation-checklist.git main:gh-pages
 
 cd -
